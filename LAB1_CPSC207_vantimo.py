403da55e411852dc66a1230f2f4234ab5e7e0465
Viviana <156944196+vaantimo@users.noreply.github.com>
2024-01-25 16:01:07 -0500

    Add files via upload

---
+++ LAB1_CPSC207_vantimo.py
@@ -0,0 +1,92 @@
+Python 3.12.1 (v3.12.1:2305ca5144, Dec  7 2023, 17:23:38) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Hello, world")
+Hello, world
+print("Welcome to CPSC 207")
+Welcome to CPSC 207
+print("Hello\nWorld")
+Hello
+World
+print("How\nare\nyou?")
+How
+are
+you?
+print("Today", "is", "Wednesday")
+Today is Wednesday
+print("Today" + "is" "Wednesday")
+TodayisWednesday
+print("a\nb\nc")
+a
+b
+c
+print("Hello" + "World")
+HelloWorld
+print("Hello\n" + "\nWorld!)
+      
+SyntaxError: unterminated string literal (detected at line 1)
+print("Hello\n" + "\nWorld!")
+      
+Hello
+
+World!
+print(123)
+      
+123
+print(45)
+      
+45
+print(6)
+      
+6
+1+2*3
+      
+7
+
+(1+2)*3
+      
+9
+7/3
+      
+2.3333333333333335
+7//3
+      
+2
+7%3
+...       
+1
+>>> 22/4
+...       
+5.5
+>>> 22//4
+...       
+5
+>>> 22%4
+...       
+2
+>>> 3**2
+...       
+9
+>>> 2**3
+...       
+8
+>>> (4 + 3)//2
+...       
+3
+>>> 6%2
+...       
+0
+>>> print(2 + 2)
+...       
+4
+>>> 1 + 2 * 3 ** 4
+...       
+163
+>>> 2 + 2 = 4
+...       
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+>>> print("2+2=", 2+2)
+...       
+2+2= 4
+>>> print("50 pounds is", 50*0.453592, "Kilograms")
+...       
+50 pounds is 22.6796 Kilograms
